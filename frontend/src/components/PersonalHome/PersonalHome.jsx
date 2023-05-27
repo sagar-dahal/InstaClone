3ce09b38138ad762fd6aa53791bcfcaad5bbc3b2
Sagar Dahal <dahal25sagar@gmail.com>
2023-05-27 03:08:01 -0400

    personal home menu bar added

--- frontend/src/components/PersonalHome/PersonalHome.jsx
+++ frontend/src/components/PersonalHome/PersonalHome.jsx
@@ -1,57 +1,60 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
+import {AiOutlineHome, AiOutlineSearch, AiOutlineHeart} from 'react-icons/ai';
+import {BsCameraReels} from 'react-icons/bs';
+import {RiMessage2Line} from 'react-icons/ri';
+import {CgProfile} from 'react-icons/cg';
+import {RxHamburgerMenu} from 'react-icons/rx';
+
+import './personalHome.css';
 
 const PersonalHome = () => {
+    const iconSize = 28;
+
   return (
-    <main className=''>
-        <div className='left-col'>
-            <div className='logo'>
-                <p className='logo'>Instagram</p>
+    <main>
+        <div className="container-l border-r-2 float-left flex flex-col">
+            <div className='content-l-1'>
+                <div className='py-4 mb-2'>
+                    <p className='logo text-3xl font-bold'>Instagram</p>
+                </div>
+                <div className='menu'>
+                    <div className='menu-home flex items-center py-4'>
+                        <div className='mr-5'> <AiOutlineHome size={iconSize} /> </div>
+                        <div className=''>Home</div>
+                    </div>
+                    <div className='menu-home flex items-center py-4 mt-2'>
+                        <div className='mr-5'> <AiOutlineSearch size={iconSize} /> </div>
+                        <div className=''>Search</div>
+                    </div>
+                    <div className='menu-home flex items-center py-4 mt-2'>
+                        <div className='mr-5'> <BsCameraReels size={iconSize} /> </div>
+                        <div className=''>Reels</div>
+                    </div>
+                    <div className='menu-home flex items-center py-4 mt-2'>
+                        <div className='mr-5'> <RiMessage2Line size={iconSize} /> </div>
+                        <div className=''>Messages</div>
+                    </div>
+                    <div className='menu-home flex items-center py-4 mt-2'>
+                        <div className='mr-5'> <AiOutlineHeart size={iconSize} /> </div>
+                        <div className=''>Notifications</div>
+                    </div>
+                    <div className='menu-home flex items-center py-4 mt-2'>
+                        <div className='mr-5'> <CgProfile size={iconSize} /> </div>
+                        <div className=''>Profile</div>
+                    </div>
+                </div>
             </div>
-            <div>
-                <Link>Home</Link>
-            </div>
-            <div>
-                <Link>Search</Link>
-            </div>
-            <div>
-                <Link>Explore</Link>
-            </div>
-            <div>
-                <Link>Reels</Link>
-            </div>
-            <div>
-                <Link>Messages</Link>
-            </div>
-            <div>
-                <Link>Notifications</Link>
-            </div>
-            <div>
-                <Link>Create</Link>
-            </div>
-            <div>
-                <Link>Profile</Link>
-            </div>
-        </div>
-
-        <div className='mid-col'>
-            <div className='stories'>
-                Stories go here
-            </div>
-            <div className='posts'>
-                Posts go here
-            </div>
-        </div>
-
-        <div className='right-col'>
-            <div className='profile'>
-                Profile img, userName, Full name goes here           
-            </div>
-            <div className='follow-suggestions'>
-                Profiles you may want to follow goes here
+            <div className='content-l-2 mt-auto flex items-center mb-5'>
+                <div className='mr-5'> <RxHamburgerMenu size={iconSize} /> </div>
+                <div className=''>More</div>
             </div>
         </div>
-      
+        <div className='container-r flex'>
+            <div>
+                sd
+            </div>
+        </div>
     </main>
   )
 }

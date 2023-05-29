ee4f9cd5048f18e245926d131662319610a74f09
Sagar Dahal <dahal25sagar@gmail.com>
2023-05-29 17:56:51 -0400

    Personal home rightmost column complete

--- frontend/src/components/PersonalHome/PersonalHome.jsx
+++ frontend/src/components/PersonalHome/PersonalHome.jsx
@@ -9,10 +9,19 @@ import {RxHamburgerMenu} from 'react-icons/rx';
 import Story from './Story';
 import Post from './Post';
 
+import dp from '../../images/dp.jpg';
+
 import './personalHome.css';
 
 const PersonalHome = () => {
     const iconSize = 28;
+    const suggestions = [
+        {url: dp, uName: 'user_1', followedBy: 'user_x + 9 more'},
+        {url: dp, uName: 'user_2', followedBy: 'user_x + 7 more'},
+        {url: dp, uName: 'user_3', followedBy: 'user_x + 3 more'},
+        {url: dp, uName: 'user_4', followedBy: 'user_x + 5 more'},
+        {url: dp, uName: 'user_5', followedBy: 'user_x + 2 more'}
+    ];
 
   return (
     <main>
@@ -53,9 +62,12 @@ const PersonalHome = () => {
                 <div className=''>More</div>
             </div>
         </div>
-        <div className='container-r flex pt-10 pl-10'>
-            <div className='media w-3/4'>
+        <div className='container-r w-9/12 mx-auto flex pt-10'>
+            <div className='r-1'>
                 <div className='stories flex'>
+                    <Story />
+                    <Story />
+                    <Story />
                     <Story />
                     <Story />
                     <Story />
@@ -63,7 +75,49 @@ const PersonalHome = () => {
                     <Story />
                 </div>
                 <div className='posts'>
-                    
+                    {/* <Post /> */}
+                </div>
+            </div>
+            <div className='r-2 pt-1 ml-14'>
+                <div className='account-switch flex items-center'>
+                    <div className='dp bg-slate-400'>
+                        <img src={dp} alt="dp" />
+                    </div>
+                    <div className='ml-5'>
+                        <Link className='font-semibold'>john_smith</Link><br />
+                        <span>John Smith</span>
+                    </div>
+                    <div className='switch-btn ml-auto'>
+                        <span className='text-blue-600 text-sm font-semibold'>Switch</span>
+                    </div>
+                </div>
+                <div className='follow-suggestions mt-4'>
+                    <div className='flex justify-between items-center'>
+                        <div>
+                            <div>
+                                <span className='font-semibold text-slate-500'>Suggested for you</span>
+                            </div>
+                        </div>
+                        <div>
+                            <Link><span className='text-sm font-semibold'>See all</span></Link>
+                        </div>
+                    </div>
+                    <div className='suggested-profiles'>
+                        {suggestions.map((item, index) => (
+                            <div className='flex items-center mt-4'>
+                                <div className='sp-dp-container'>
+                                    <img src={item.url} alt={item.uName} className='sp-dp' key={index} />
+                                </div>
+                                <div className='ml-3 leading-4'>
+                                    <Link className='font-semibold'>{item.uName}</Link><br />
+                                    <span className='text-xs'>Followed by {item.followedBy}</span>
+                                </div>
+                                <div className='switch-btn ml-auto'>
+                                    <span className='text-blue-600 text-sm font-semibold'>Follow</span>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
